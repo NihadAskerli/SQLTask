bd9ccfe888d4c5ed27d49de84a98ffa803157a69
Nihad <esgerlinihad7@gmail.com>
2022-10-26 21:26:24 +0400

    change

---
+++ FirstProject.sql
@@ -0,0 +1,62 @@
+select * from currency_n;
+select * from loancsv;
+create table AZN as (select * from loancsv where 2=1);
+select * from AZN;
+create table dollar as (select * from loancsv where 2=1);
+create table tl as (select * from loancsv where 2=1);
+create table rubul as (select * from loancsv where 2=1);
+insert all 
+when currency_id=11 then into AZN
+WHEN currency_id=12 THEN into dollar
+when currency_id=13 then into tl
+when currency_id=14 then into rubul
+select * from loancsv;
+CREATE TABLE members (
+    member_id NUMBER PRIMARY KEY,
+    first_name VARCHAR2(50) NOT NULL,
+    last_name VARCHAR2(50) NOT NULL,
+    rank VARCHAR2(20)
+);
+
+CREATE TABLE member_staging AS 
+SELECT * FROM members;
+-- insert into members table    
+INSERT INTO members(member_id, first_name, last_name, rank) VALUES(1,'Abel','Wolf','Gold');
+INSERT INTO members(member_id, first_name, last_name, rank) VALUES(2,'Clarita','Franco','Platinum');
+INSERT INTO members(member_id, first_name, last_name, rank) VALUES(3,'Darryl','Giles','Silver');
+INSERT INTO members(member_id, first_name, last_name, rank) VALUES(4,'Dorthea','Suarez','Silver');
+INSERT INTO members(member_id, first_name, last_name, rank) VALUES(5,'Katrina','Wheeler','Silver');
+INSERT INTO members(member_id, first_name, last_name, rank) VALUES(6,'Lilian','Garza','Silver');
+INSERT INTO members(member_id, first_name, last_name, rank) VALUES(7,'Ossie','Summers','Gold');
+INSERT INTO members(member_id, first_name, last_name, rank) VALUES(8,'Paige','Mcfarland','Platinum');
+INSERT INTO members(member_id, first_name, last_name, rank) VALUES(9,'Ronna','Britt','Platinum');
+INSERT INTO members(member_id, first_name, last_name, rank) VALUES(10,'Tressie','Short','Bronze');
+
+-- insert into member_staging table
+INSERT INTO member_staging(member_id, first_name, last_name, rank) VALUES(1,'Abel','Wolf','Silver');
+INSERT INTO member_staging(member_id, first_name, last_name, rank) VALUES(2,'Clarita','Franco','Platinum');
+INSERT INTO member_staging(member_id, first_name, last_name, rank) VALUES(3,'Darryl','Giles','Bronze');
+INSERT INTO member_staging(member_id, first_name, last_name, rank) VALUES(4,'Dorthea','Gate','Gold');
+INSERT INTO member_staging(member_id, first_name, last_name, rank) VALUES(5,'Katrina','Wheeler','Silver');
+INSERT INTO member_staging(member_id, first_name, last_name, rank) VALUES(6,'Lilian','Stark','Silver');
+select * from members;
+select * from member_staging;
+
+select * from members 
+left join member_staging on members.member_id=member_staging.member_id;
+MERGE INTO member_staging x
+USING (SELECT member_id, first_name, last_name, rank FROM members) y
+ON (x.member_id  = y.member_id)
+WHEN MATCHED THEN
+    UPDATE SET x.first_name = y.first_name, 
+                        x.last_name = y.last_name, 
+                        x.rank = y.rank
+    WHERE x.first_name <> y.first_name OR 
+           x.last_name <> y.last_name OR 
+           x.rank <> y.rank 
+WHEN NOT MATCHED THEN
+    INSERT(x.member_id, x.first_name, x.last_name, x.rank)  
+    VALUES(y.member_id, y.first_name, y.last_name, y.rank);
+    
+
+
